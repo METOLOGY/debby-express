3ff9e6264bbd26ea50be69ae1ad71335905f348d
Weian Wang <stargazer.wang@gmail.com>
2017-06-23 23:20:06 +0800

    shop page under construction...

---
+++ components/console/Shop.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="shop-layout">
+    <div class="building-wrap">
+      <img src="~assets/svg/girl.svg" alt="">
+      <h5>還在施工中...</h5>
+      <h5>敬請期待！</h5>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Shop'
+}
+</script>
+
+<style lang="sass" scoped>
+h5
+  margin: 10px 0px
+  
+
+.shop-layout
+  height: calc(100vh - 55px - 40px)
+  display: flex
+  .building-wrap
+    margin: auto 
+    text-align: center
+
+</style>
