66bade5aa3a64f1fc1e61de7463ee974dead4c69
Weiann <stargazer.wang@gmail.com>
2017-06-27 11:58:00 +0800

    redirect url for heroku url

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
 <script>
 import axios from '~plugins/axios'
 
-const redirectUri = 'http://localhost:3000'
+const redirectUri = 'http://limitless-shore-51089'
 
 let lineLoginBaseUrl = 'https://access.line.me/dialog/oauth/weblogin?'
 lineLoginBaseUrl += 'response_type=code'
